f9d38e3627daa7e9f7987d071d258d4357ee8454
Anton Korobeynikov <anton@korobeynikov.info>
2013-02-08 13:23:17 +0400

    Command / response buffer is 4 bytes, not 1.

--- Connections/MSPBSL_Connection5xx.cpp
+++ Connections/MSPBSL_Connection5xx.cpp
@@ -140,7 +140,7 @@ uint16_t MSPBSL_Connection5xx::eraseSegment(uint32_t addr)
 uint16_t MSPBSL_Connection5xx::TX_BufferSize(uint16_t* bufSize)
 {
   uint16_t retValue;
-  uint8_t command[1];
+  uint8_t command[4];
   uint8_t rxBuf[4];
   command[0]=TX_BUFFER_SIZE_COMMAND;
   retValue = thePacketHandler->TX_Packet(command, 1);
